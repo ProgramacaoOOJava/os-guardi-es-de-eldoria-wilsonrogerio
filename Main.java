a57b7d31b1615fe0b8890da4dfe00495e364229d
wilsonrogerio <108371734+wilsonrogerio@users.noreply.github.com>
2026-07-20 12:33:33 +0000

    adicionado codigo do nivel novato

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        //Crio o personagem
+        Personagem heroi1 = new Personagem("Wilson", "Mago", 400, 99, 13.0);
 
-        .
-        .
-        .
-
+        //Chama o metodo da classe para exibir os status
+        heroi1.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -7,17 +7,21 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
 
     }
 
     public void exibirStatus() {
+        //Saida dos status formatada
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.println("Nome: " + this.nome); 
+        System.out.println("Classe: " + this.classe); 
+        System.out.println("Nível: " + this.nivel); 
+        System.out.println("Pontos de Vida: " + this.pontosDeVida); 
+        System.out.println("Poder Base: " + this.poderBase); 
 
     }
 }
